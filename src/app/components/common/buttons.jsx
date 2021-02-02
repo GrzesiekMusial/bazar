cfd385bb91d4d174247b7c108174ffae72902a25
Gregor1494 <grzesiekmusial94@gmail.com>
2021-02-02 10:51:32 +0100

    Button Ripple added.

--- src/app/components/common/buttons.jsx
+++ src/app/components/common/buttons.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Ripples from "react-ripples";
 
 const Buttons = ({ buttons }) => {
     return (
         <div className="screen__buttons">
             {buttons.map((button, index) => (
-                <NavLink key={`nav-${index}`} to={button.ref}>
-                    <span
-                        style={{
-                            visibility: "visible",
-                            position: "fixed",
-                            bottom: "-500px",
-                        }}
-                    >
-                        {button.info}
-                    </span>
-                    {button.name}
-                </NavLink>
+                <Ripples>
+                    <NavLink key={`nav-${index}`} to={button.ref}>
+                        <span
+                            style={{
+                                visibility: "visible",
+                                position: "fixed",
+                                bottom: "-500px",
+                            }}
+                        >
+                            {button.info}
+                        </span>
+                        {button.name}
+                    </NavLink>
+                </Ripples>
             ))}
         </div>
     );
